2859acc95d6be8524a82c3ca647a669536535eb6
Priyankk18k <priyankk18k@gmail.com>
2018-01-20 16:38:16 +0530

    Addition of new functionalities

--- main.py
+++ main.py
@@ -1,59 +1,47 @@
+from spy_details import spy_rating,spy_age,spy_name,spy_salutation,spy_is_online
 print "Hello!"
 
 print 'Let\'s get started'
 
-spy_name = input("Welcome to spy chat, you must tell me your spy name first: ")
+print"Welcome to SpyChat"
+question="Do you want to continue as " + spy_salutation + " " + spy_name + " (Y/N)? "
+existing=raw_input(question)
+def start_chat(spy_name,spy_age, spy_rating):
 
-if len(spy_name) > 0:
-    # To verify the length of the spy
+    if spy_age > 12 and spy_age < 50:
+     print "Authentication complete. Welcome %s  age:%d    and rating of: %.2f   Proud to have you onboard" % (spy_name, spy_age, spy_rating)
+    # Showing  whether the authentication is complete or not.
 
-    print 'Welcome ' + spy_name + '. Glad to have you back with us.'
-
-    spy_salutation = raw_input("Should I call you Mister or Miss?: ")
-    # Raw_input becasue to get the string value only
-
-    spy_name = spy_salutation + " " + spy_name
-    # Print spy salutation and spy name and store it in the spy_name
-
-    print "Alright " + spy_name + ". I'd like to know a little bit more about you before we proceed..."
-
-    spy_age = 0
-    spy_rating = 0.0
-    spy_is_online = False
-    # Initializing the values .
-
-    spy_age = int(input("What is your age? Please give a valid number"))
-    # Taking input from the user and converting the input to integer values
-
-    while(spy_age):
-
-        if spy_age > 12 and spy_age < 50:
-
-            spy_rating = input("What is your spy rating? Please give a valid Rating number")
-
-            if spy_rating > 4.5:
-                print 'Great ace!'
-            elif spy_rating > 3.5 and spy_rating <= 4.5:
-                print 'You are one of the good ones.'
-            elif spy_rating >= 2.5 and spy_rating <= 3.5:
-                print 'You can always do better'
-            else:
-                print 'We can always use somebody to help in the office.'
-
-            spy_is_online = True
-            # Showing spy is online or not
-
-
-            print "Authentication complete. Welcome %s  age:%d    and rating of: %f   Proud to have you onboard" % (spy_name,spy_age,spy_rating)
-             # Showing  whether the authentication is complete or not.
-
+     menu_choice = "What do you want to do ?\n 1-ADD a Status Update\n 2-QUIT"
+     menu_choice = input(menu_choice)
+     if menu_choice == 1:
+         print("Status is updated")
+     else:
+         pass
 
 
 
     else:
-        print 'Sorry you are not of the correct age to be a spy'
-
-
+     print 'Sorry you are not of the correct age to be a spy'
+if existing == "Y":
+    start_chat(spy_name,spy_age, spy_rating)
 else:
+    spy_name = ''
+    spy_salutation = ''
+    spy_age = 0
+    spy_rating = 0.0
+    spy_is_online = False
+    spy_name = raw_input("Welcome to spy chat, you must tell me your spy name first: ")
 
-    print "A spy needs to have a valid name. Try again please."
+    if len(spy_name) > 0:
+        spy_salutation = raw_input("Should I call you Mr. or Ms.?: ")
+
+        spy_age = input("What is your age?")
+
+        spy_rating = input("What is your spy rating?")
+
+        spy_is_online = True
+        start_chat(spy_name, spy_age, spy_rating)
+
+    else:
+        print 'Please add a valid spy name'
---
+++ spy_details.py
@@ -0,0 +1,9 @@
+spy_name = 'bond'
+
+spy_salutation = 'Mr.'
+
+spy_age = 23
+
+spy_rating = 4.8
+
+spy_is_online = True
